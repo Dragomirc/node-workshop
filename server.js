5a1ece4f83a00d6dd0e38fa19e409f03d061cb01
dragomir <svedava@mail.ru>
2017-11-13 14:13:59 +0000

    created a handler

--- server.js
+++ server.js
@@ -1,5 +1,11 @@
 var http = require('http');
-var server = http.createServer();
+var server = http.createServer(handler);
 server.listen(3000,function(){
   console.log('"Server is listenting on port 3000.Ready to accept requests"');
 });
+var message = 'I\'m so happy to be part of the Node Girls workshop!';
+function handler(request,response){
+  response.writeHead(200,{"Content-Type": "text/html"});
+  response.write(message);
+  response.end();
+}
